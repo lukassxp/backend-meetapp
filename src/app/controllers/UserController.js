e426021960d1b4e61d30fb9e5c94f89dc4ba28cc
lukassxp <lpalucas.10@gmail.com>
2019-08-30 21:42:00 -0300

    created endpoint to update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,29 @@ class UserController {
 
     return res.json({ id, name, email });
   }
+
+  async update(req, res) {
+    const id = req.userId;
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(id);
+
+    if (email && user.email !== email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email already in use!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Password does not match!' });
+    }
+
+    const { name } = await user.update(req.body);
+
+    return res.json({ id, name, email });
+  }
 }
 
 export default new UserController();

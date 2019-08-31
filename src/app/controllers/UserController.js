8b3aa8e8119d31b8b03959963f524a04f5df98e1
lukassxp <lpalucas.10@gmail.com>
2019-08-30 22:15:44 -0300

    created validantion to update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -32,6 +32,30 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) => {
+          return oldPassword ? field.required() : field;
+        }),
+      confirmPassword: Yup.string()
+        .min(6)
+        .when('password', (password, field) => {
+          return password
+            ? field.required().oneOf([Yup.ref('password')])
+            : field;
+        }),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(400)
+        .json({ error: 'Body of request out of expected format!' });
+    }
+
     const id = req.userId;
     const { email, oldPassword } = req.body;
 
